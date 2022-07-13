bcce77a0034424abc0a862c89bc0873f7923ae15
Aji Hanggoro <adjihans@gmail.com>
2022-07-13 08:34:00 +0700

    change App styling

--- jobkita/src/App.js
+++ jobkita/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./Routes";
 
 const App = () => {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes />
+      </Router>
+    </div>
   );
 };
 
