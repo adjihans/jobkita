447e20cce08c22162be7e26ab856005a0dd93ecb
Aji Hanggoro <adjihans@gmail.com>
2022-07-14 00:44:13 +0700

    add navbar to job create

--- jobkita/src/pages/JobCreate.js
+++ jobkita/src/pages/JobCreate.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { POST_JOB } from "../config/schema";
 import { useHistory } from "react-router";
+import Navbar from "../components/Navbar";
 
 export const JobCreate = () => {
   const history = useHistory();
@@ -58,37 +59,40 @@ export const JobCreate = () => {
 
   return (
     <div>
-      <input
-        placeholder="Company Name"
-        value={form.companyName}
-        onChange={(event) => onChange(event, "companyName")}
-      />
-      <input
-        placeholder="Company Address"
-        value={form.companyAddress}
-        onChange={(event) => onChange(event, "companyAddress")}
-      />
-      <input
-        placeholder="Job Title"
-        value={form.jobTitle}
-        onChange={(event) => onChange(event, "jobTitle")}
-      />
-      <input
-        placeholder="Job Description"
-        value={form.jobDescription}
-        onChange={(event) => onChange(event, "jobDescription")}
-      />
-      <input
-        placeholder="User Email"
-        value={form.userEmail}
-        onChange={(event) => onChange(event, "userEmail")}
-      />
-      <input
-        placeholder="URL"
-        value={form.applyUrl}
-        onChange={(event) => onChange(event, "applyUrl")}
-      />
-      <button onClick={handleOnClickCreate}>Create</button>
+      <Navbar showBorder={true} backgroundColor="#fff" />
+      <div>
+        <input
+          placeholder="Company Name"
+          value={form.companyName}
+          onChange={(event) => onChange(event, "companyName")}
+        />
+        <input
+          placeholder="Company Address"
+          value={form.companyAddress}
+          onChange={(event) => onChange(event, "companyAddress")}
+        />
+        <input
+          placeholder="Job Title"
+          value={form.jobTitle}
+          onChange={(event) => onChange(event, "jobTitle")}
+        />
+        <input
+          placeholder="Job Description"
+          value={form.jobDescription}
+          onChange={(event) => onChange(event, "jobDescription")}
+        />
+        <input
+          placeholder="User Email"
+          value={form.userEmail}
+          onChange={(event) => onChange(event, "userEmail")}
+        />
+        <input
+          placeholder="URL"
+          value={form.applyUrl}
+          onChange={(event) => onChange(event, "applyUrl")}
+        />
+        <button onClick={handleOnClickCreate}>Create</button>
+      </div>
     </div>
   );
 };
